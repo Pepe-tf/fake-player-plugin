43e1d929fd7649616ccaaba74051a6b8f8ca116a
Kyttu <46562317+KyttuXD@users.noreply.github.com>
2026-05-03 18:15:43 -0400

    Delete src/main directory

--- src/main/java/me/bill/fakePlayerPlugin/api/FppBotTickHandler.java
+++
@@ -1,8 +0,0 @@
-package me.bill.fakePlayerPlugin.api;
-
-import org.jetbrains.annotations.NotNull;
-
-@FunctionalInterface
-public interface FppBotTickHandler {
-  void onTick(@NotNull FppBot bot, @NotNull org.bukkit.entity.Player entity);
-}
